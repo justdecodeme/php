8158bbb462ab115db3097c7996980d4034f25305
rakesh <justdecodeme@hotmail.com>
2018-02-07 00:38:58 +0530

    updating crud pdo

--- references/crud-pdo.php
+++ references/crud-pdo.php
@@ -4,26 +4,23 @@
   defined('DB_PASS') or define('DB_PASS', '');
   defined('DB_NAME') or define('DB_NAME', 'testdb');
 
-  # MySQLi Procedural connection
+  # PDO connection
 
   # 1. Connect to Database
-  $connection = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);;
-  //
-  // if($connection) {
-  //   echo "Connected to Database: " . DB_NAME;
-  // } else {
-  //   die("ERROR " . mysqli_connect_error());
-  // }
+  try {
+     $connection = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+     // Error Handling
+     $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     
+     echo "connected";
+     $connection = null;
+
+  }
+  catch(PDOException $error){
+      print "Error ". $error->getMessage() ;
+      die();
+  }
 
   # 2. Create Database
-  // $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS);
-  //
-  // if($connection) {
-  //   echo "Connected";
-  // } else {
-  //   die("ERROR " . mysqli_connect_error());
-  // }
-  //
-  // $query = "CREATE DATABASE mydba";
-  // mysqli_query($connection, $query);
+ 
 ?>
