fa8383175a28e110069a1678b96c460c1542ad5c
rakesh <justdecodeme@hotmail.com>
2018-02-09 01:17:20 +0530

    code indented

--- projects/cms/includes/header.php
+++ projects/cms/includes/header.php
@@ -1,33 +1,33 @@
 <header class="navbar navbar-inverse navbar-static-top" >
-			<div class="container">
-				<a href="index.php" class="navbar-brand">CMS System</a>
-				<ul class="nav navbar-nav navbar-right">
-					
-					<?php
-						// Make navigation and add class as per active page
+	<div class="container">
+		<a href="index.php" class="navbar-brand">CMS System</a>
+		<ul class="nav navbar-nav navbar-right">
+			
+			<?php
+				// Make navigation and add class as per active page
 
-						// separately check as this don't belong to any category as above
-						$class = $_SERVER['PHP_SELF'] == '/projects/cms/index.php' ? 'active' : '';
-						echo '<li class="' . $class .'"><a href="index.php">Home</a>';
-						
-						// add active class to category if selected
-						$sel_cat = "SELECT * FROM category";
-						$run_cat = mysqli_query($conn, $sel_cat);
-						while($rows = mysqli_fetch_assoc($run_cat)) {
-							if(isset($_GET['cat_id'])) {
-								$class = $_GET['cat_id'] == $rows['c_id'] ? 'active' : '';
-							} else {
-								$class='';
-							}
-							echo '<li class="'.$class.'"><a href="menu.php?cat_id='.$rows['c_id'].'">'.ucfirst($rows['category_name']).'</a></li>';
-						}
+				// separately check as this don't belong to any category as above
+				$class = $_SERVER['PHP_SELF'] == '/projects/cms/index.php' ? 'active' : '';
+				echo '<li class="' . $class .'"><a href="index.php">Home</a>';
+				
+				// add active class to category if selected
+				$sel_cat = "SELECT * FROM category";
+				$run_cat = mysqli_query($conn, $sel_cat);
+				while($rows = mysqli_fetch_assoc($run_cat)) {
+					if(isset($_GET['cat_id'])) {
+						$class = $_GET['cat_id'] == $rows['c_id'] ? 'active' : '';
+					} else {
+						$class='';
+					}
+					echo '<li class="'.$class.'"><a href="menu.php?cat_id='.$rows['c_id'].'">'.ucfirst($rows['category_name']).'</a></li>';
+				}
 
-						// separately check as they don't belong to any category as above
-						$class = $_SERVER['PHP_SELF'] == '/projects/cms/contact.php' ? 'active' : '';
-						echo '<li class="' . $class .'"><a href="contact.php">Contact Us</a>';
-						$class = $_SERVER['PHP_SELF'] == '/projects/cms/registration.php' ? 'active' : '';
-						echo '<li class="' . $class .'"><a href="registration.php">Registration</a>';
-					?>
-				</ul>
-			</div>
-		</header>
+				// separately check as they don't belong to any category as above
+				$class = $_SERVER['PHP_SELF'] == '/projects/cms/contact.php' ? 'active' : '';
+				echo '<li class="' . $class .'"><a href="contact.php">Contact Us</a>';
+				$class = $_SERVER['PHP_SELF'] == '/projects/cms/registration.php' ? 'active' : '';
+				echo '<li class="' . $class .'"><a href="registration.php">Registration</a>';
+			?>
+		</ul>
+	</div>
+</header>
