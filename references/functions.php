e96c0bd3669760526bf5d6db7fb80f370ffd4dc6
rakesh <justdecodeme@hotmail.com>
2018-02-08 12:06:01 +0530

    more types of functions added

--- references/functions.php
+++ references/functions.php
@@ -1,7 +1,26 @@
 <?php 
+	// simple function
 	function sayHi() {
-		echo "Hi from function!";
+		echo "Hi from function! <br>";
 	}
-
 	sayHi();
+
+	// function with arguments
+	function sayName($fname) {
+	    echo "Welcome $fname <br>";
+	}
+	sayName("Sofia");	
+
+	// function with default arguments
+	function square($x = 3) {
+	    echo "Square of $x is " . $x*$x . '<br>';
+	}
+	square(4);	
+	square();	
+
+	// function with returning statements
+	function sum($x = 3, $y = 4) {
+	    return $x + $y;
+	}
+	echo "5 + 7 is " . sum(5, 7) . '<br>'
  ?>
---
+++ references/variables.php
@@ -0,0 +1,6 @@
+<?php 
+	# Global Variables
+	######################
+
+	$_Ser
+ ?>
