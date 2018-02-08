e4a348e6963b3d9a5f004f7d67b14046aa0ead4b
rakesh <justdecodeme@hotmail.com>
2018-02-08 11:48:04 +0530

    associative arrays added

--- references/arrays.php
+++ references/arrays.php
@@ -1,4 +1,22 @@
 <?php
+
+	# Indexed arrays - Arrays with a numeric index
 	$subjects = array('php', 'nodejs', 'python');
-	echo $subjects[0];
+	echo $subjects[0] . '<br>';
+	# Loop Through an Indexed Array
+	$arrlength = count($subjects);
+
+	for($x = 0; $x < $arrlength; $x++) {
+	    echo $subjects[$x];
+	    echo "<br>";
+	}	
+
+	# Associative arrays - Arrays with named keys
+	$subjectsLevel = array("php"=>"easy", "nodejs"=>"medium", "js"=>"very easy");
+	echo $subjectsLevel['php'] . '<br>';
+	# Loop Through an Associative Array
+	foreach($subjectsLevel as $key => $value) {
+	    echo "Key=" . $key . ", Value=" . $value;
+	    echo "<br>";
+	}	
 ?>
