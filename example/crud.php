d56128fc1af01df04e64c724de89be267cdeaa64
rakesh <justdecodeme@hotmail.com>
2018-02-08 07:49:17 +0530

    making full crud file

---
+++ example/crud.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CRUD</title>
+	<style>
+		h1 {
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<h1>CRUD</h1>
+
+	<form action="" method="post">
+		<input type="text" name="name" placeholder="name"><br>
+		<input type="email" name="email" placeholder="email"><br>
+		<input type="password" name="password" placeholder="password"><br>
+		<input type="submit" name="submit" value="Submit">
+	</form>
+</body>
+</html>
