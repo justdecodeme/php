e362a88b664dd4894895962321ecf81bc47499f5
rakesh <justdecodeme@hotmail.com>
2018-03-03 22:56:44 +0530

    font-awesome icons added

--- projects/cms-video/video_sharing_portal/index.php
+++ projects/cms-video/video_sharing_portal/index.php
@@ -23,19 +23,19 @@
 			<div class="categories">
 				<h4>Categories</h4>
 				<div class="nav flex-column nav-pills" aria-orientation="vertical">
-				  <a class="nav-link active" href="#">HTML</a>
-				  <a class="nav-link" href="#">css</a>
-				  <a class="nav-link" href="#">JS</a>
-				  <a class="nav-link" href="#">PHP</a>
+				  <a class="nav-link active" href="#"><i class="fab fa-html5"></i> HTML</a>
+				  <a class="nav-link" href="#"><i class="fab fa-css3-alt"></i> css</a>
+				  <a class="nav-link" href="#"><i class="fab fa-js"></i> JS</a>
+				  <a class="nav-link" href="#"><i class="fab fa-php"></i> PHP</a>
 				</div>
 			</div>
 			<div class="my-activity" style="margin-top: 20px;">
 				<h4>My Account</h4>
 				<div class="nav flex-column nav-pills" aria-orientation="vertical">
-				  <a class="nav-link active" href="#">My Channel</a>
-				  <a class="nav-link" href="#">My Playlist</a>
-				  <a class="nav-link" href="#">My Liked Videos</a>
-				  <a class="nav-link" href="#">My Subscribed Channels</a>
+				  <a class="nav-link active" href="#"><i class="fa fa-video"></i>  Channel</a>
+				  <a class="nav-link" href="#"><i class="fa fa-list-ul"></i>  Playlist</a>
+				  <a class="nav-link" href="#"><i class="fa fa-heart"></i>  Liked Videos</a>
+				  <a class="nav-link" href="#"><i class="fa fa-plus-square"></i>  Subscribed Channels</a>
 				</div>
 			</div>
 		</div>
