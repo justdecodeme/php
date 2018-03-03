1663b81cf97af65ecaa509ec65ed3358a26d4a18
rakesh <justdecodeme@hotmail.com>
2018-03-03 20:23:43 +0530

    post of the day added

--- projects/cms-video/index.php
+++ projects/cms-video/index.php
@@ -99,7 +99,18 @@
 
 				<div class="post-of-the-day">
 					<h2>Post of the Day</h2>
-
+					<div class="outer">
+						<h4>The Second Post</h4>
+						<p><a href="post.php">date</a> | <a href="post.php">Technology</a> | <a href="post.php">Rakko</a></p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis repellat fuga, eveniet quo inventore quibusdam, saepe ratione placeat nemo consequuntur, eveniet quo inventore quibusdam, saepe ratione placeat nemo consequuntur, repudiandae praesentiu... <a href="post.php">Read More</a>
+						</p>
+						<p class="categories">
+							<a href="#" class="btn btn-light btn-sm">What is Technology?</a>
+							<a href="#" class="btn btn-light btn-sm">How technology works?</a>
+							<a href="#" class="btn btn-light btn-sm">Intro to Technology?</a>
+						</p>
+					</div>
 				</div>
 			</div>
 		</div>
