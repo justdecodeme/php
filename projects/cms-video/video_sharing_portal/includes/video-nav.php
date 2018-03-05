ad1b1b750df534088d20d73c568bd22c965d6a31
rakesh <justdecodeme@hotmail.com>
2018-03-05 21:25:53 +0530

    login.php page added

--- projects/cms-video/video_sharing_portal/includes/video-nav.php
+++ projects/cms-video/video_sharing_portal/includes/video-nav.php
@@ -5,10 +5,10 @@
         <a class="nav-link" href="#">Upload <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#"><i class="fa fa-sign-in-alt"></i> Singin</a>
+        <a class="nav-link" href="login.php"><i class="fa fa-sign-in-alt"></i> Singin</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#"><i class="fa fa-user-plus"></i> Join</a>
+        <a class="nav-link" href="register.php"><i class="fa fa-user-plus"></i> Join</a>
       </li>
     </ul>
     </form>
---
+++ projects/cms-video/video_sharing_portal/login.php
@@ -0,0 +1,26 @@
+<?php include '../includes/header.php' ?>
+
+<?php include 'includes/video-nav.php' ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<form action="">
+				<h2>Log in</h2>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="text" class="form-control" id="email">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="text" class="form-control" id="password">
+				</div>
+				<div class="form-group">
+					<button class="btn btn-primary btn-block">Submit</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<?php include '../includes/footer.php' ?>
