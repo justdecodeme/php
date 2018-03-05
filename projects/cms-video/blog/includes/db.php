f3ed66de390ed789560cac846015f35521e2e7b7
rakesh <justdecodeme@hotmail.com>
2018-03-05 22:54:52 +0530

    using prepared statements to load post from database

--- projects/cms-video/blog/includes/db.php
+++ projects/cms-video/blog/includes/db.php
@@ -9,12 +9,17 @@
   # 1. Connect to Database
   ##############################
   try {
-    $connection = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
-    // Error Handling
-    $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $attrs is optional, this demonstrates using persistent connections,
+    // the equivalent of mysql_pconnect
+    $attrs = array(PDO::ATTR_PERSISTENT => true);
+
+    $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS, $attrs);
+
+    // the following tells PDO we want it to throw Exceptions for every error.
+    // this is far more useful than the default mode of throwing php errors
+    $pdo->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
     // echo "Connected to Database: " . DB_NAME;
-    $connection = null;
   } catch(PDOException $error) {
     print "Error ". $error->getMessage() ;
     die();
--- projects/cms-video/blog/index.php
+++ projects/cms-video/blog/index.php
@@ -11,6 +11,30 @@
 
 	<div class="row">
 		<div class="col-md-8">
+
+			<?php 
+				// prepare the statement. the place holders allow PDO to handle substituting
+				// the values, which also prevents SQL injection
+				// $stmt = $pdo->prepare("SELECT * FROM product WHERE productTypeId=:productTypeId AND brand=:brand");
+				$stmt = $pdo->prepare("SELECT * FROM blog_data");			
+
+				// // bind the parameters
+				// $stmt->bindValue(":productTypeId", 6);
+				// $stmt->bindValue(":brand", "Slurm");				
+
+				// initialise an array for the results 
+				// $blog_post = array();
+				if ($stmt->execute()) {
+				    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+				        // $blog_post[] = $row;
+					    echo "id:".$row["blog_id"]." category:".$row["blog_category"]."<br>";
+				    }
+				}
+
+				// set PDO to null in order to close the connection
+				$pdo = null;
+			 ?>
+
 			<div class="post">
 				<h1>The First Post</h1>
 				<p><a href="post.php">date</a> | <a href="post.php">Technology</a> | <a href="post.php">Rakko</a></p>
@@ -23,30 +47,7 @@
 					<a href="label.php" class="btn btn-light btn-sm">Intro to Technology?</a>
 				</p>
 			</div>
-			<div class="post">
-				<h1>The Second Post</h1>
-				<p><a href="post.php">date</a> | <a href="post.php">Technology</a> | <a href="post.php">Rakko</a></p>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis repellat fuga, eveniet quo inventore quibusdam, saepe ratione placeat nemo consequuntur, eveniet quo inventore quibusdam, saepe ratione placeat nemo consequuntur, repudiandae praesentiu... <a href="post.php">Read More</a>
-				</p>
-				<p>
-					<a href="#" class="btn btn-light btn-sm">What is Technology?</a>
-					<a href="#" class="btn btn-light btn-sm">How technology works?</a>
-					<a href="#" class="btn btn-light btn-sm">Intro to Technology?</a>
-				</p>
-			</div>
-			<div class="post">
-				<h1>The Third Post</h1>
-				<p><a href="post.php">date</a> | <a href="post.php">Technology</a> | <a href="post.php">Rakko</a></p>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis repellat fuga, eveniet quo inventore quibusdam, saepe ratione placeat nemo consequuntur, eveniet quo inventore quibusdam, saepe ratione placeat nemo consequuntur, repudiandae praesentiu... <a href="post.php">Read More</a>
-				</p>
-				<p>
-					<a href="#" class="btn btn-light btn-sm">What is Technology?</a>
-					<a href="#" class="btn btn-light btn-sm">How technology works?</a>
-					<a href="#" class="btn btn-light btn-sm">Intro to Technology?</a>
-				</p>
-			</div>
+
 		</div>
 
 		<?php include 'includes/blog-sidebar.php' ?>
