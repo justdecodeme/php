2fef8b6cc8e43aad59eb183614160c6f67d8dfff
Rakesh <hashrakeshkumar@gmail.com>
2018-03-08 20:18:16 +0530

    column updated

--- projects/x_connect/index.php
+++ projects/x_connect/index.php
@@ -7,23 +7,23 @@
 <div class="container">
   <h2>Dashboard</h2>
   <div class="row">
-    <div class="col-md-3">
+    <div class="col-md-4">
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">WD-05</h5>
           <h6 class="card-subtitle mb-2 text-muted">10:00 AM - 12:00 PM</h6>
-          <p class="card-text">Rakesh</p>
+          <p class="card-text">Trainer: Rakesh</p>
           <a href="#" class="card-link">Edit</a>
           <a href="#" class="card-link">Delete</a>
         </div>
       </div>
     </div>
-    <div class="col-md-3">
+    <div class="col-md-4">
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">GR-09</h5>
           <h6 class="card-subtitle mb-2 text-muted">02:30 PM - 04:30 PM</h6>
-          <p class="card-text">Asha</p>
+          <p class="card-text">Trainer: Asha</p>
           <a href="#" class="card-link">Edit</a>
           <a href="#" class="card-link">Delete</a>
         </div>
