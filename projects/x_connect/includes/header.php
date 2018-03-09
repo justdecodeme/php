fd853df799404c7429823b6f338fdaa6bd2adf28
Rakesh <hashrakeshkumar@gmail.com>
2018-03-09 14:50:24 +0530

    navbar added

--- projects/x_connect/includes/header.php
+++ projects/x_connect/includes/header.php
@@ -10,3 +10,20 @@
     <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
   </head>
   <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="index.php">xConnect</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="signup.php">Signup</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
