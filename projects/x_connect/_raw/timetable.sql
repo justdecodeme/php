1f0e20bc6255eadd6ab3f5c51eb405bb339039da
Rakesh <hashrakeshkumar@gmail.com>
2018-03-11 21:15:33 +0530

    titetable database backup

--- projects/x_connect/_raw/timetable.sql
+++ projects/x_connect/_raw/timetable.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 10, 2018 at 03:07 PM
+-- Generation Time: Mar 11, 2018 at 04:44 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `timetable` (
   `instructor_code` varchar(255) NOT NULL,
   `start_time` time NOT NULL,
   `end_time` time NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=78 ;
 
 --
 -- Dumping data for table `timetable`
@@ -44,21 +44,24 @@ CREATE TABLE IF NOT EXISTS `timetable` (
 INSERT INTO `timetable` (`id`, `batch_code`, `date`, `class_code`, `room_code`, `instructor_code`, `start_time`, `end_time`) VALUES
 (1, 'gr180325', '2018-03-25', 'gr1', 'a', 'ins_rakesh', '11:30:00', '13:30:00'),
 (2, 'gr180325', '2018-04-01', 'gr2', 'b', 'ins_vinay', '09:00:00', '11:00:00'),
-(3, 'gr180325', '2018-04-01', 'gr3', 'd', 'ins_varsha', '11:30:00', '13:30:00'),
-(9, 'u180325', '2018-03-09', 'u1', 'a', 'ins_aish', '09:30:00', '13:30:00'),
-(10, 'u180325', '2018-03-11', 'u2', 'b', 'ins_varsha', '11:00:00', '13:00:00'),
-(11, 'u180325', '2018-03-13', 'u3', 'a', 'ins_aish', '09:00:00', '11:00:00'),
-(17, 'u180325', '2018-03-09', 'ur1', 'b', 'ins_varsha', '09:30:00', '13:30:00'),
-(24, 'bc180305', '2018-03-10', 'bc2', 'b', 'ins_pallavi', '02:01:00', '13:30:00'),
-(29, 'bc180305', '2018-03-15', 'bc5', 'b', 'ins_asha', '09:30:00', '13:30:00'),
-(38, 'bc180305', '2018-03-09', 'bc1', 'a', 'ins_aish', '09:30:00', '13:30:00'),
+(3, 'gr180325', '2018-04-01', 'gr3', 'a', 'ins_varsha', '11:30:00', '13:30:00'),
 (40, 'gr180325', '2018-04-08', 'grp1', 'a', 'ins_vinay', '09:00:00', '11:00:00'),
 (41, 'gr180325', '2018-04-08', 'gr4', 'a', 'ins_pallavi', '11:30:00', '13:30:00'),
 (42, 'gr180325', '2018-04-15', 'gr6', 'a', 'ins_rakesh', '11:30:00', '13:30:00'),
 (43, 'gr180325', '2018-04-15', 'gr5', 'a', 'ins_vinay', '09:00:00', '11:00:00'),
 (44, 'gr180325', '2018-04-22', 'grp2', 'a', 'ins_vinay', '09:00:00', '11:00:00'),
 (45, 'gr180325', '2018-04-22', 'gr7', 'a', 'ins_rakesh', '11:30:00', '13:30:00'),
-(46, 'gr180325', '2018-04-29', 'gr8', 'a', 'ins_vinay', '09:00:00', '11:00:00');
+(46, 'gr180325', '2018-04-29', 'gr8', 'a', 'ins_vinay', '09:00:00', '11:00:00'),
+(68, 'php180325', '2018-03-06', 'php8', 'a', 'ins_vinay', '09:00:00', '11:00:00'),
+(69, 'php180325', '2018-03-09', 'phpp1', 'a', 'ins_asha', '09:00:00', '11:00:00'),
+(70, 'php180325', '2018-03-13', 'phpp2', 'a', 'ins_vinay', '09:00:00', '11:00:00'),
+(71, 'php180325', '2018-03-16', 'phpp3', 'a', 'ins_asha', '09:00:00', '11:00:00'),
+(72, 'php180325', '2018-03-20', 'phpp4', 'a', 'ins_vinay', '09:00:00', '11:00:00'),
+(73, 'php180325', '2018-03-23', 'phpp5', 'a', 'ins_asha', '09:00:00', '11:00:00'),
+(74, 'u180325', '2018-03-11', 'up1', 'b', 'ins_varsha', '09:00:00', '11:00:00'),
+(75, 'u180325', '2018-03-11', 'ue1', 'b', 'ins_varsha', '11:30:00', '13:30:00'),
+(76, 'u180325', '2018-03-25', 'u1', 'b', 'ins_varsha', '09:00:00', '11:00:00'),
+(77, 'u180325', '2018-03-26', 'ur1', 'b', 'ins_varsha', '11:30:00', '13:30:00');
 
 --
 -- Indexes for dumped tables
@@ -78,7 +81,7 @@ ALTER TABLE `timetable`
 -- AUTO_INCREMENT for table `timetable`
 --
 ALTER TABLE `timetable`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=47;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=78;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
