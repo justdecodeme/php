de344868543931c7a4bb2e570c52e94ea196d4d9
Rakesh <hashrakeshkumar@gmail.com>
2018-03-16 22:48:10 +0530

    student feedback basic ui done

--- projects/x_connect/feedback_student.php
+++ projects/x_connect/feedback_student.php
@@ -6,54 +6,32 @@
 ?>
 
 <div class="container" id="attendanceOuter">
-  <h2>Feedback</h2>
+  <h2>Feedback Student</h2>
   <hr>
 
   <div class="row">
-    <div class="col-md-4">
-      <form class="form-block">
-        <label for="selectedBatch">Select Batch</label>
-        <select class="custom-select my-1" id="currentBatch">
-          <option value="bc180305" data-template="bootcamp" selected>bc180305 (Bootcamp)</option>
-          <option value="u180325" data-template="unity">u180325 (Unity)</option>
-          <option value="gr180325" data-template="graphic" >gr180325 (Graphic Design)</option>
-          <option value="php180325" data-template="php" >php180325 (PHP & MySQL)</option>
-        </select>
-      </form>
-    </div>
-    <div class="col-md-2">
-      <form class="form-block">
-        <label for="selectedBatch">Select Class</label>
-        <select class="custom-select my-1" id="currentClass">
-          <option value="bc1">BC 1</option>
-          <option value="bc2">BC 2</option>
-          <option value="bc3">BC 3</option>
-          <option value="bc4">BC 4</option>
-        </select>
-      </form>
-    </div>
-    <div class="col-md-3">
-      <label for="selectedBatch">Timing</label>
-      <form class="form-block" style="display: flex;">
-        <input type="time" disabled class="form-control" id="startClassTime" value="11:00" style="margin-right: 10px;">
-        <input type="time" disabled class="form-control" id="endClassTime" value="13:00">
-      </form>
-    </div>
-    <div class="col-md-2">
-      <div class="button-group selectStudentsDropdown">
-        <label for="">Select Students</label>
-        <button type="button" class="btn btn-default btn-block dropdown-toggle" data-toggle="dropdown">0</button>
-        <ul class="dropdown-menu">
-         <li><a href="#" data-value="std130308" tabIndex="-1"><input type="checkbox"/>&nbsp;Nagraj</a></li>
-         <li><a href="#" data-value="std130301" tabIndex="-1"><input type="checkbox"/>&nbsp;Sachin</a></li>
-         <li><a href="#" data-value="std130208" tabIndex="-1"><input type="checkbox"/>&nbsp;Bhagya</a></li>
-         <li><a href="#" data-value="std130108" tabIndex="-1"><input type="checkbox"/>&nbsp;Amit</a></li>
-        </ul>
-       </div>
-    </div>
-    <div class="col-md-1">
-      <label style="visibility: hidden;">.</label>
-      <button class="btn btn-outline-danger">Submit</button>
+    <div class="col-md-6 offset-md-3">
+        <div class="feedback-outer text-center">
+          <div class="ins-img">
+            <img src="./_assets/images/feedback.jpeg" class="img-fluid">
+          </div>
+          <div class="detail">
+            <span class="name">Rakesh</span> |
+            <span class="class">WD 01</span> |
+            <span class="timing">12th Mar 18 (Mon)</span>
+          </div>
+          <div class="rating">
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+            <i class="fa fa-star"></i>
+          </div>
+          <div class="comment">
+            <textarea name="name" class="form-control" placeholder="comments..."></textarea>
+          </div>
+          <input type="submit" class="btn btn-outline-success" value="Submit">
+        </div>
     </div>
   </div>
 </div>
