4a0bbedea268b3f52573858aee7775d3ca114714
rakesh <justdecodeme@hotmail.com>
2018-03-18 15:06:54 +0530

    common header added to each page

--- projects/registration_system_email_verification/activate.php
+++ projects/registration_system_email_verification/activate.php
@@ -1,8 +1,8 @@
 <?php include('includes/header.php'); ?>
 <?php include('includes/nav.php'); ?>
 
-  <div class="jumbotron">
-    <h1 class="text-center">Activate</h1>
-  </div>
+<div class="jumbotron">
+	<h1 class="text-center">Activate</h1>
+</div>
 
 <?php include('includes/footer.php'); ?>
