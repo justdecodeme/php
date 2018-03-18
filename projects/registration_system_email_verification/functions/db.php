bcac1dd9616e66192f3c4d9f7702424e2ab9f24e
rakesh <justdecodeme@hotmail.com>
2018-03-18 16:18:39 +0530

    added functions

--- projects/registration_system_email_verification/functions/db.php
+++ projects/registration_system_email_verification/functions/db.php
@@ -1,3 +1,33 @@
 <?php 
+	$con = mysqli_connect('localhost', 'root', '', 'login_db');
 
- ?>
+	if(!$con) {
+		echo 'Not connected to DB';
+	}
+
+	function escape($string) {
+		global $con;
+		
+		return mysqli_real_escape_string($con, $string);
+	}
+
+	function query($query) {
+		global $con;
+
+		return mysqli_query($con, $query);
+	}
+
+	function fetch_array($result) {
+		global $con;
+
+		return mysqli_fetch_array($result);
+	}
+
+	function confirm($result) {
+		global $con;
+
+		if(!$result) {
+			die('QUERY FAILED: ' . mysqli_error($con));
+		}
+	}
+?>
