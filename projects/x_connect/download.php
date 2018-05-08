b4da8ff6c29557767c7f3014a72ddc944265435e
Rakesh <justdecodeme@hotmail.com>
2018-05-08 15:30:40 +0530

    Download: UI updated

---
+++ projects/x_connect/download.php
@@ -0,0 +1,39 @@
+<?php
+  include 'includes/header.php';
+?>
+
+<div class="container downlod-page">
+  <h2>Select your prefered platform!</h2>
+  <hr>
+
+  <div class="row">
+    <div class="col-md-4 offset-md-2">
+      <div class="list-group" style="margin-bottom: 20px;">
+        <a href="#" class="list-group-item list-group-item-action list-group-item-success">
+          <span class="platform-icon"><i class="fab fa-android"></i></span> Android <span class="download-icon"><i class="fa fa-download"></i></span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action list-group-item-info">
+          <span class="platform-icon"><i class="fas fa-mobile-alt"></i></span> iPhone <span class="download-icon"><i class="fa fa-download"></i></span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action list-group-item-secondary">
+          <span class="platform-icon"><i class="fab fa-windows"></i></span> Windows Phone <span class="download-icon"><i class="fa fa-download"></i></span>
+        </a>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action list-group-item-success">
+          <span class="platform-icon"><i class="fas fa-desktop"></i></span> PC <span class="download-icon"><i class="fa fa-download"></i></span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action list-group-item-info">
+          <span class="platform-icon"><i class="fas fa-laptop"></i></span> Mac <span class="download-icon"><i class="fa fa-download"></i></span>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action list-group-item-secondary">
+          <span class="platform-icon"><i class="fab fa-linux"></i></span> Linux <span class="download-icon"><i class="fa fa-download"></i></span>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
